7776e98dfcebe4432e5ebfdf140e6284e7022cf8
resnyanskiy <resnyanskiy@gmail.com>
2013-06-21 15:40:32 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+﻿resnyanskiy.js.tree
+=======
+Super simple open-source TreeView component for web-pages based on HTML, CSS and JavaScript, implemented with TypeScript and LESS.
+
+It can be described with several "NoXXX" abstracts (means "not"):
+
+- **NoLegacy**. No support for IE9- browsers, thinking in ES5+, CSS3 and other [modern technologies](http://www.html5rocks.com/).
+- **NoJQuery**. No framework used, only [VanillaJS](http://www.vanilla-js.com/).
+- **NoComplex**. No support for complex use cases, only one simple way to use.
+
+What about "YesXXX"?
+
+- Everything you need for create "treeview" based on data in JSON,  including lazy loading for subnodes. Data model for tree node (TypeScript):
+
+        interface ITreeNodeDataModel {
+          id: number;
+          title: string;
+          isBranch: bool;
+        }
+
+- Using [TypeScript](http://www.typescriptlang.org/) and [LESS](http://www.lesscss.org/) for implementation, no external dependencies, modular structure (including CSS rules) and clear code base make it simple to use in your own projects and customization.
+
+For "how to use" information take a look at comprehensive [example]().
---
+++ resnyanskiy.js.tree.ts
@@ -0,0 +1,174 @@
+module Resnyanskiy {
+  //#region shortcuts for build-in functions
+  function createElement(tagName: string, className?: string, id?: string) {
+    var result = document.createElement(tagName);
+    if(className) result.className = className;
+    if(id) result.id = id;
+    return result;
+  }
+  //#endregion
+
+  // IComponent
+  export interface ITreeNodeDataModel {
+    id: number; // positive integer, 0 for root node (invisible by default)
+    title: string;
+    isBranch: bool;
+  }
+
+  export interface TreeNodeCollection extends Object {
+    [id: number]: TreeNode;
+  }
+
+  // IComposite
+  // interface ITreeNode extends ITreeNodeDataModel {
+  //   addItem(item: ITreeNode): void;
+  //   removeItem(id: number): void;
+  //   findItem(id: number, deep: bool): ITreeNode;
+  // }
+  export class TreeNode implements ITreeNodeDataModel {
+    constructor(public id: number, public title: string, public isBranch: bool) { }
+
+    private items: TreeNodeCollection = <TreeNodeCollection>{};
+
+    //#region IComposite
+    public addItem(item: TreeNode) {
+      var itemId: number = item.id;
+      if(this.findItem(itemId, false) == null) {
+        this.items[itemId] = item;
+      }
+    }
+
+    public removeItem(id: number) {
+      delete this.items[id];
+    }
+
+    public findItem(id: number, deep: bool): TreeNode {
+      var result: TreeNode = this.items[id];
+
+      if(!deep || result)
+        return result;
+
+      for(var n in this.items)
+        return this.items[n].findItem(id, deep);
+    }
+    //#endregion
+
+    public getItems(): TreeNodeCollection {
+      return this.items;
+    }
+
+    public hasItems(): bool {
+      return Object.keys(this.items).length > 0;
+    }
+
+    public getView(): HTMLElement {
+      var listNodeContent = createElement("a");
+      listNodeContent.setAttribute("href", "#");
+      listNodeContent.appendChild(document.createTextNode(this.title));
+
+      var listNode: HTMLElement = createElement("span", this.isBranch ? "branch" : null, "li-" + this.id);
+      listNode.appendChild(createElement("span", "connector"));
+      listNode.appendChild(createElement("span", "icon"));
+      listNode.appendChild(listNodeContent);
+
+      var listItem: HTMLElement = createElement("li");
+      listItem.appendChild(listNode);
+
+      if(this.isBranch)
+        listItem.appendChild(createElement("ul", null, "ul-" + this.id));
+
+      return listItem;
+    }
+  }
+
+  export class Tree {
+    private treeContainer: HTMLElement;
+    private rootNode: TreeNode;
+
+    private toggleBranchItemsVisibleClosure: (ev: Event) => void;
+    private onNodeClickClosure: (ev: Event) => void;
+
+    constructor(container: HTMLElement, nodes?: TreeNode[]) {
+      //#region events closures
+      this.toggleBranchItemsVisibleClosure = (ev: Event) => {
+        var nodeId: number = parseInt(/\d+$/.exec((<HTMLElement>ev.target).parentNode.attributes["id"].value)[0], 10);
+        var node: TreeNode = this.rootNode.findItem(nodeId, true)
+        if(!this.toggleNodeItemsVisible(node) && (this.onBranchExpand instanceof Function)) {
+          (<HTMLElement> this.treeContainer.querySelector("li > span#li-" + nodeId)).classList.add("loading");
+          this.onBranchExpand(nodeId);
+        }
+      };
+
+      this.onNodeClickClosure = (ev: Event) => {
+        var nodeId: number = parseInt(/\d+$/.exec((<HTMLElement>ev.target).parentNode.attributes["id"].value)[0], 10);
+        if(this.onNodeClick instanceof Function)
+          this.onNodeClick(nodeId);
+      };
+      //#endregion
+
+      this.treeContainer = createElement("ul", "container");
+
+      this.rootNode = new TreeNode(0, "root", true);
+      if(nodes)
+        for(var n in nodes)
+          this.rootNode.addItem(nodes[n]);
+
+      this.renderNodeItemsTo(this.treeContainer, this.rootNode);
+
+      container.classList.add("resnyanskiy-tree");
+      container.appendChild(this.treeContainer);
+    }
+
+    private renderNodeItemsTo(ulElement: Element, node: TreeNode) {
+      var nodeItems: TreeNodeCollection = node.getItems();
+      for(var id in nodeItems) {
+        var nodeItem: TreeNode = nodeItems[id];
+        ulElement.appendChild(nodeItem.getView());
+      }
+
+      var branchConnectorNodeList: NodeList = ulElement.querySelectorAll("span.branch > span.connector");
+      for(var i = 0; i < branchConnectorNodeList.length; i++) {
+        var connectorNode: Node = branchConnectorNodeList[i];
+        connectorNode.addEventListener("click", this.toggleBranchItemsVisibleClosure);
+      }
+
+      var contentNodeList: NodeList = ulElement.querySelectorAll("span > a");
+      for(var i = 0; i < contentNodeList.length; i++) {
+        var contentNode: Node = contentNodeList[i];
+        contentNode.addEventListener("click", this.onNodeClickClosure);
+      }
+    }
+
+    private toggleNodeItemsVisible(node: TreeNode): bool {
+      if(node && node.hasItems()) {
+        var ulElement: Element = this.treeContainer.querySelector("ul#ul-" + node.id);
+        if(ulElement.hasChildNodes()) {
+          while(ulElement.firstChild) ulElement.removeChild(ulElement.firstChild);
+          (<HTMLElement>ulElement.previousSibling).classList.remove("opened");
+        }
+        else {
+          this.renderNodeItemsTo(ulElement, node);
+          (<HTMLElement>ulElement.previousSibling).classList.add("opened");
+        }
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+
+    public updateNode(id: number, items: TreeNodeCollection, showNodeItems?: bool) {
+      var node: TreeNode = this.rootNode.findItem(id, true);
+      for(var i in items)
+        node.addItem(items[i]);
+
+      (<HTMLElement> this.treeContainer.querySelector("li > span#li-" + id)).classList.remove("loading");
+
+      if(showNodeItems)
+        this.toggleNodeItemsVisible(node);
+    }
+
+    public onBranchExpand: (treeNodeId: number) => void;
+    public onNodeClick: (treeNodeId: number) => void;
+  }
+}
